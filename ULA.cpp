b43298fc0d55860417db62471921b300f8064274
Samuel Jales <samueljjales@gmail.com>
2025-04-04 16:41:41 -0300

    ler arquivo

--- ULA.cpp
+++ ULA.cpp
@@ -1,2 +1,25 @@
 #include "ULA.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void ULA:: lerarquivo(string nomearquivo){
+    ifstream arquivo(nomearquivo); // abre o arquivo para leitura
+
+    if (!arquivo.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo!" << std::endl;
+    }
+
+    string linha;
+    while (std::getline(arquivo, linha)) {
+        IR = linha;
+        PC = PC +1;
+    }
+
+    
+
+    
+
+    arquivo.close(); // fecha o arquivo
+}
--- ULA.h
+++ ULA.h
@@ -1,12 +1,23 @@
 #ifndef ULA_H
 #define ULA_H
 #include <iostream>
+#include <string>
+using namespace std;
 
 class ULA {
     public:
         bool F0, F1, ENA, ENB, INVA, INC;
-    private:
-        
+        bool A, B;
+        string IR;
+        int PC;
+        int S;
+
+        void lerarquivo(string arquivo);
+
+    ULA(){
+        A = 1;
+        B = 1;
+    }
 };
 
 #endif
